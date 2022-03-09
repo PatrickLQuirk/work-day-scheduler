93591f909c8ec9a24a7ec9e80cd5cc479c33696d
Patrick Quirk <patrickliamquirk@yahoo.com>
2022-03-09 13:12:54 -0600

    refactored to use jquery

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,41 +1,40 @@
-var timeBlockContainerEl = document.querySelector("#time-block-container");
+var timeBlockContainerEl = $("#time-block-container");
 
 var createTimeBlocks = function() {
     // in the for loop, i is the number of hours since midnight (the hour in 24-hour time)
     for (i = 9; i <= 17; i++) {
-        var timeBlockEl = document.createElement("div");
-        timeBlockEl.className = "time-block row";
         
-        // create a time variable that gives the time of the block in the format (hour)(AM/PM)
+        // create a blockHour variable that gives the hour of the block in the format (hour)(AM/PM)
         // (i.e. 10AM or 3PM)
         if (i < 12) {
-            var time = i + "AM";
+            var blockHour = i + "AM";
         }
         else if (i === 12) {
-            var time = i + "PM";
+            var blockHour = i + "PM";
         }
         else {
-            var time = (i - 12) + "PM";
+            var blockHour = (i - 12) + "PM";
         }
 
-        timeBlockEl.setAttribute("data-time", time);
+        var timeBlock = $("<div>")
+            .addClass("time-block row")
+            .attr("data-time", blockHour);
         
-        hourEl = document.createElement("h2");
-        hourEl.textContent = time;
-        hourEl.className = "hour col-1";
-        timeBlockEl.appendChild(hourEl);
+        var hourH2 = $("<h2>")
+            .addClass("hour col-1")
+            .text(blockHour);
         
-        eventDescripEl = document.createElement("textarea");
-        // may remove the past class from this declaration and handle the time separately
-        eventDescripEl.className = "description col-10 past";
-        timeBlockEl.appendChild(eventDescripEl);
+        var eventDescription = $("<textarea>").addClass("description col-10 past");
 
-        saveButtonEl = document.createElement("button");
-        saveButtonEl.className = "saveBtn col-1";
-        saveButtonEl.innerHTML = "<i class='fas fa-save'></i>";
-        timeBlockEl.appendChild(saveButtonEl);
+        var saveButton = $("<button>")
+            .addClass("saveBtn col-1");
+        var saveButtonIcon = $("<i>")
+            .addClass("fas fa-save");
+        saveButton.append(saveButtonIcon);
 
-        timeBlockContainerEl.appendChild(timeBlockEl);
+        timeBlock.append(hourH2, eventDescription, saveButton);
+
+        timeBlockContainerEl.append(timeBlock);
     };
 };
 
