65deae81fa287e330945c19e156a1f26902f2ef9
Patrick Quirk <patrickliamquirk@yahoo.com>
2022-03-11 10:26:08 -0600

    comments on JavaScript plus minor tweaks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,28 @@
 var timeBlockContainerEl = $("#time-block-container");
+
+// object to store the events that the user adds
+// the keys are the hours of the day and the values are the event descriptions
+// hours where the timeblock has no event description are not stored in the object
+//      (there is no key associated with them)
 var events = {}
 
 // takes the hour in 24 hour time (i) and outputs a string that gives the hour in the format (hour)(AM/PM)
 // (i.e. 10 becomes "10AM" and 15 becomes "3PM")
 // the purpose of having the i is to make looping through the hours of a business day easier.
 var convertHourFormat = function(i) {
-    if (i < 12) {
-        return i + "AM";
-    }
-    else if (i === 12) {
-        return i + "PM";
-    }
-    else {
-        return (i - 12) + "PM";
-    };
+    var hourMoment = moment().hour(i);
+    return hourMoment.format("hA");
 }
+
+// create the timeblock for a specific hour
+// utilizes the eventHour and eventText from loadTasks()
 var createTimeBlock = function(eventHour, eventText) {
+    // create the timeblock
     var timeBlock = $("<div>")
         .addClass("time-block row")
         .attr("data-time", eventHour);
     
+    // create p element for displaying the hour of the timeblock
     var hourP = $("<p>")
         .addClass("hour col-1")
         .text(eventHour);
@@ -33,22 +36,30 @@ var createTimeBlock = function(eventHour, eventText) {
         .addClass("fas fa-save");
     saveButton.append(saveButtonIcon);
 
+    // append all three elements to the timeblock
     timeBlock.append(hourP, eventDescription, saveButton);
 
+    // color-code the timeblock before adding it to the page
     auditTimeBlock(timeBlock);
-    displayDate();
 
     timeBlockContainerEl.append(timeBlock);
 };
 
+// color-code a time-block
 var auditTimeBlock = function(timeBlock) {
     var blockHourText = $(timeBlock).attr("data-time");
-    var blockHour = moment(blockHourText, "hA").hour();
-    var currentHour = moment().hour();
-    
+
     // remove old classes from timeBlock textarea
     var timeBlockText = $(timeBlock).find("textarea");
     $(timeBlockText).removeClass("past present future");
+
+    // Define moments for the hour corresponding to the block and for the current time,
+    //      then get just the hour of the day (in 24-hour time).
+    // The two hours are then compared to decide how to color the timeblocks
+    // Doing it this way instead of using moment's isAfter and isBefore methods
+    //      makes it easier to correctly color the block for the current hour.
+    var blockHour = moment(blockHourText, "hA").hour();
+    var currentHour = moment().hour();
 
     if (blockHour < currentHour) {
         $(timeBlockText).addClass("past");
@@ -59,6 +70,7 @@ var auditTimeBlock = function(timeBlock) {
     }
 }
 
+// display the current date in the header
 var displayDate = function() {
     var currentDate = moment().format("dddd, MMMM Do");
     $("#currentDay").text(currentDate);
@@ -72,6 +84,8 @@ var loadEvents = function() {
 
     for (i = 9; i <= 17; i++) {
         var eventHour = convertHourFormat(i);
+        // if we have event text stored in events for that hour, then use that text
+        // otherwise use an empty string
         if (events[eventHour]) {
             var eventText = events[eventHour];
         }
@@ -94,10 +108,13 @@ setInterval(function() {
     });
 }, 1000 * 60);
 
+// update the date displayed at the top of the page every hour
 setInterval(function() {
     displayDate();
-}, 1000 * 60 * 60 * 6);
+}, 1000 * 60 * 60);
 
+// event listener for the save buttons
+// saves the events when triggered
 $("#time-block-container").on("click", "button", function() {
     var eventBlock = $(this).closest(".time-block");
     var eventHour = $(eventBlock).attr("data-time");
@@ -105,6 +122,7 @@ $("#time-block-container").on("click", "button", function() {
         .val().trim();
     events[eventHour] = eventText;
     saveEvents();
-})
+});
 
+displayDate();
 loadEvents();
